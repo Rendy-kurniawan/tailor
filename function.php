11283ac30616983719cd1563ecfb592dbc41d462
Rendy-kurniawan <kurniawanrendy53@gmail.com>
2019-07-29 10:10:12 +0700

    update aplikasi bagian email
    Signed-off-by: Rendy-kurniawan <kurniawanrendy53@gmail.com>

--- function.php
+++ function.php
@@ -6,7 +6,7 @@ $dbuser = "osaru_tech";
 $dbpass = "Sr123Th24"; */
 
 
-$baseurl = "http://localhost/tailor/";	
+$baseurl = "http://localhost/tailor/";
 $dbname = "tailor";
 $dbhost = "localhost";
 $dbuser = "root";
@@ -17,11 +17,11 @@ error_reporting(E_ALL);
 function connectdb()
 {
     global $dbname, $dbuser, $dbhost, $dbpass;
-    $conms = @mysql_connect($dbhost,$dbuser,$dbpass); //connect mysql
-	mysql_set_charset('utf8', $conms);
-    if(!$conms) return false;
+    $conms = @mysql_connect($dbhost, $dbuser, $dbpass); //connect mysql
+    mysql_set_charset('utf8', $conms);
+    if (!$conms) return false;
     $condb = @mysql_select_db($dbname);
-    if(!$condb) return false;
+    if (!$condb) return false;
     return true;
 }
 
@@ -29,94 +29,90 @@ function connectdb()
 
 function dbconnect()
 {
-	global $pdo;
+    global $pdo;
 
-	try {
-		$pdo = new PDO('mysql:host='.$GLOBALS['dbhost'].';dbname='.$GLOBALS['dbname'].'', $GLOBALS['dbuser'], $GLOBALS['dbpass']);
-	} catch (PDOException $e) {
-		die('MySQL connection fail! ' . $e->getMessage());
-	}
+    try {
+        $pdo = new PDO('mysql:host=' . $GLOBALS['dbhost'] . ';dbname=' . $GLOBALS['dbname'] . '', $GLOBALS['dbuser'], $GLOBALS['dbpass']);
+    } catch (PDOException $e) {
+        die('MySQL connection fail! ' . $e->getMessage());
+    }
 }
 
 
 function insert_new_user($username, $password)
 {
-	# checking username is already taken
-	if (username_exists($username))
-		return false;
+    # checking username is already taken
+    if (username_exists($username))
+        return false;
 
-	# insert new user info
-	global $pdo;
-	$stmt = $pdo->prepare('
+    # insert new user info
+    global $pdo;
+    $stmt = $pdo->prepare('
 		INSERT INTO users
 		(username, password)
 		values (:username, :password)');
 
-	$stmt->execute( array(':username' => $username, ':password' => md5($password)) );
+    $stmt->execute(array(':username' => $username, ':password' => md5($password)));
 
-	if ($pdo->lastInsertId())
-		return true;
-	else
-		return false;
+    if ($pdo->lastInsertId())
+        return true;
+    else
+        return false;
 }
 
 function username_exists($username)
 {
-	global $pdo;
-	
-	$stmt = $pdo->prepare('
+    global $pdo;
+
+    $stmt = $pdo->prepare('
 		SELECT id
 		FROM users
 		WHERE username = :username
 		LIMIT 1');
 
-	$stmt->execute( array('username' => $username) );
-	return $stmt->fetchColumn();
+    $stmt->execute(array('username' => $username));
+    return $stmt->fetchColumn();
 }
 
 function attempt($username, $password)
 {
-	global $pdo;
-	
-	$stmt = $pdo->prepare('
+    global $pdo;
+
+    $stmt = $pdo->prepare('
 		SELECT id, username
 		FROM users
 		WHERE username = :username AND password = :password
 		LIMIT 1');
 
-	$stmt->execute(array(':username' => $username, 'password' => md5($password)));
+    $stmt->execute(array(':username' => $username, 'password' => md5($password)));
 
-	if ($data = $stmt->fetch( PDO::FETCH_OBJ )) {
-		# set session
-		$_SESSION['username'] = $data->username;
-		return true;
-	} else {
-		return false;
-	}
+    if ($data = $stmt->fetch(PDO::FETCH_OBJ)) {
+        # set session
+        $_SESSION['username'] = $data->username;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function is_user()
 {
-	if (isset($_SESSION['username']))
-		return true;
+    if (isset($_SESSION['username']))
+        return true;
 }
 
 function redirect($url)
 {
-	header('Location: ' .$url);
-	exit;
+    header('Location: ' . $url);
+    exit;
 }
 
-function valid_username($str){
-	return preg_match('/^[a-z0-9_-]{3,16}$/', $str);
+function valid_username($str)
+{
+    return preg_match('/^[a-z0-9_-]{3,16}$/', $str);
 }
 
-function valid_password($str){
-	return preg_match('/^[a-z0-9_-]{6,18}$/', $str);
+function valid_password($str)
+{
+    return preg_match('/^[a-z0-9_-]{6,18}$/', $str);
 }
-
-
-
-
-
-?>
